f617eed860b91cd6d4b7e5ba774d015684fe5d39
helenamin <helen.amin30@yahoo.com>
2021-07-18 16:35:21 +0800

    working on charts

--- static/js/national.js
+++ static/js/national.js
@@ -1,39 +1,147 @@
 //Define api urls
-var install_url = `/api/v1.0/install`
-var rebate_url = `/api/v1.0/rebate`
-var suburbs_url = `/api/v1.0/suburbs`
-var income_url = `/api/v1.0/income`
-// var output_url = `/api/v1.0/output`
-
-
-
+var state_url = `/api/v1.0/state`
 
 // Initializes the page with a default plot
 function init() {
 
-    d3.json(install_url).then(function(ins_data) {
-        d3.json(rebate_url).then(function(reb_data) {
-            d3.json(suburbs_url).then(function(sub_data) {
-                d3.json(income_url).then(function(inc_data) {
-                    //Test json data
-                    console.log(ins_data);
-                    console.log(reb_data);
-                    console.log(sub_data);
-                    console.log(inc_data);
+    d3.json(state_url).then(function(data) {
 
-                    // buildPlot(data);
-                });
-            });
-        });
+        // // Test json data
+        console.log(data);
+
+
+
+        state_ins ={}
+        state_out ={}
+        state_ins_cum ={}
+        state_out_cum ={}
+
+        data.forEach(st => {
+            ins_his = [];
+            out_his =[];
+            ins_cum_his = [];
+            out_cum_his =[];
+            ins = 0;
+            out = 0;
+            ins_years = st.install.years;
+            out_years = st.output.years;
+            years = Object.keys(st.install.years);
+
+            for (key in ins_years){
+                ins_his.push(ins_years[key]);
+                out_his.push(out_years[key]);
+                ins += ins_years[key];
+                out += out_years[key];
+                // console.log(ins)
+                ins_cum_his.push(ins);
+                out_cum_his.push(out);
+            }
+
+            state_ins[st.state] = ins_his
+            state_out[st.state] = out_his
+
+            state_ins_cum[st.state] = ins_cum_his
+            state_out_cum[st.state] = out_cum_his
+
+        })
+
+        console.log(years)
+
+        for (i=0;i<years.length;i++){
+            years[i] = parseInt(years[i]);
+        }
+
+        console.log(state_ins_cum);
+        console.log(state_out);
+
+        console.log(years)
+     
+        console.log(state_ins.ACT);
+       
+        buildLinePlot(years,state_ins_cum,"cumulative Installation");
+
+        buildLinePlot(years,state_out_cum,"Output");
+
     });
     
 }
 
 
-// // Plot the bubble chart
-// function buildPlot(data){  
+// Plot the line chart
+function buildLinePlot(years,state_data,data_type){  
+    var act_trace = {
+        x: years,
+        y: state_data.ACT,
+        mode : 'lines',
+        name: 'ACT'
+    };
+    
+    var nsw_trace = {
+        x: years,
+        y: state_data.NSW,
+        mode: 'lines',
+        name: 'NSW'
+    };
 
-// }
+    var nt_trace = {
+        x: years,
+        y: state_data.NT,
+        mode: 'lines',
+        name: 'NT'
+    };
+    
+    var qld_trace = {
+        x: years,
+        y: state_data.QLD,
+        mode: 'lines',
+        name: 'QLD'
+    };
+
+    var sa_trace = {
+        x: years,
+        y: state_data.SA,
+        mode: 'lines',
+        name: 'SA'
+    };
+
+    var tas_trace = {
+        x: years,
+        y: state_data.TAS,
+        mode: 'lines',
+        name: 'TAS'
+    };
+
+    var vic_trace = {
+        x: years,
+        y: state_data.VIC,
+        mode: 'lines',
+        name: 'VIC'
+    };
+
+    var wa_trace = {
+        x: years,
+        y: state_data.WA,
+        mode: 'lines',
+        name: 'WA'
+    };
+
+    var national_trace = {
+        x: years,
+        y: state_data.National,
+        mode: 'lines',
+        name: 'National'
+    };
+
+    var data = [act_trace, nsw_trace, nt_trace, qld_trace, sa_trace, tas_trace, vic_trace, wa_trace,national_trace];
+
+    var layout = {
+        title:`States comparison on ${data_type} over years`
+      };
+
+    Plotly.newPlot(`${data_type} line chart`,data, layout);
+}
+
+
 
 
 init();
